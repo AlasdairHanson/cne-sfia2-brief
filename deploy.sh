38bfbe26db8e8759a422b55329e0b49e4159b314
AlasdairHanson <71394754+AlasdairHanson@users.noreply.github.com>
2020-11-20 13:51:38 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 ssh -tt -i /home/jenkins/.ssh/id_rsa ubuntu@52.211.44.119 << EOF
-git clone https://github.com/AlasdairHanson/cne-sfia2
+git clone https://github.com/AlasdairHanson/cne-sfia2-brief
 export DATABASE_URI=terraform-20201120093727436000000002.clh36ghk8zgg.eu-west-1.rds.amazonaws.com
 export TEST_DATABASE_URI=terraform-20201120093727462000000003.clh36ghk8zgg.eu-west-1.rds.amazonaws.com
 docker-compose --build
-docker exec backend bash -c "pytest tests/ --cov application
-docker exec frontend bash -c "pytest tests/ --cov application
+docker exec backend bash -c "pytest tests/ --cov application >> backend_test.txt
+docker exec frontend bash -c "pytest tests/ --cov application >> frontend_test.txt
 docker-compose down
 EOF
