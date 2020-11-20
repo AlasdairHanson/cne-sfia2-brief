c1babb6ab997415975f857838b19c395e1787c5d
AlasdairHanson <71394754+AlasdairHanson@users.noreply.github.com>
2020-11-20 22:00:33 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,7 @@ git clone https://github.com/AlasdairHanson/cne-sfia2-brief
 export DATABASE_URI='mysql+pymysql://dbProject:dB4a15!4@terraform-20201120185648967400000003.clh36ghk8zgg.eu-west-1.rds.amazonaws.com:3306/k8sdb'
 export TEST_DATABASE_URI='mysql+pymysql://dbProject:dB4a15!4@terraform-20201120185648962100000002.clh36ghk8zgg.eu-west-1.rds.amazonaws.com:3306/testdb'
 
-mysql -h terraform-20201120185648962100000002.clh36ghk8zgg.eu-west-1.rds.amazonaws.com -P 3306 -u dbProject -p < cne-sfia2-brief/database/Create.sql
-dB4a15!4
+mysql -h terraform-20201120185648962100000002.clh36ghk8zgg.eu-west-1.rds.amazonaws.com -P 3306 -u dbProject -p dB4a15!4 < cne-sfia2-brief/database/Create.sql
 
 sudo -E DATABASE_URI=$DATABASE_URI, TEST_DATABASE_URI=$TEST_DATABASE_URI docker-compose up -d
 
