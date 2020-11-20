c5c9a380e4ff20146e56a3929265b768b797828f
AlasdairHanson <71394754+AlasdairHanson@users.noreply.github.com>
2020-11-20 21:58:45 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,15 @@ sudo rm -r cne-sfia2-brief
 
 git clone https://github.com/AlasdairHanson/cne-sfia2-brief
 
-export DATABASE_URI=mysql+pymysql://root:dB4a15!4@terraform-20201120185648967400000003.clh36ghk8zgg.eu-west-1.rds.amazonaws.com:3306/k8sdb
-export TEST_DATABASE_URI=mysql+pymysql://root:dB4a15!4@terraform-20201120185648962100000002.clh36ghk8zgg.eu-west-1.rds.amazonaws.com:3306/testdb
+export DATABASE_URI='mysql+pymysql://dbProject:dB4a15!4@terraform-20201120185648967400000003.clh36ghk8zgg.eu-west-1.rds.amazonaws.com:3306/k8sdb'
+export TEST_DATABASE_URI='mysql+pymysql://dbProject:dB4a15!4@terraform-20201120185648962100000002.clh36ghk8zgg.eu-west-1.rds.amazonaws.com:3306/testdb'
 
 mysql -h terraform-20201120185648962100000002.clh36ghk8zgg.eu-west-1.rds.amazonaws.com -P 3306 -u dbProject -p < cne-sfia2-brief/database/Create.sql
 
-docker-compose up -d
+sudo -E DATABASE_URI=$DATABASE_URI, TEST_DATABASE_URI=$TEST_DATABASE_URI docker-compose up -d
 
-docker exec backend bash -c "pytest tests/test_backend.py --cov application >> backend_test.txt
-docker exec frontend bash -c "pytest tests/test_frontend.py --cov application >> frontend_test.txt
+docker exec backend bash -c "pytest tests/ --cov application >> backend_test.txt
+docker exec frontend bash -c "pytest tests/ --cov application >> frontend_test.txt
 
 docker-compose down
 EOF
