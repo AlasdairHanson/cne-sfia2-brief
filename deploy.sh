e6197c3ca24bc34623dab395910c764001d0f6af
AlasdairHanson <71394754+AlasdairHanson@users.noreply.github.com>
2020-11-21 12:26:32 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ mysql -h terraform-20201120185648967400000003.clh36ghk8zgg.eu-west-1.rds.amazona
 #TEST DATABASE
 mysql -h terraform-20201120185648962100000002.clh36ghk8zgg.eu-west-1.rds.amazonaws.com -P 3306 -u dbProject -p'dB4a15!4' < cne-sfia2-brief/database/Create.sql
 
-sudo -E DATABASE_URI=$DATABASE_URI, TEST_DATABASE_URI=$TEST_DATABASE_URI docker-compose up -d
+sudo -E DATABASE_URI=$DATABASE_URI TEST_DATABASE_URI=$TEST_DATABASE_URI docker-compose up -d
 
 docker exec backend bash -c "pytest tests/ --cov application" >> backend_test.txt
 docker exec frontend bash -c "pytest tests/ --cov application" >> frontend_test.txt
